35488160a601594f94d161d6b253b36752a0066c
viki <viki@gmail.com>
2024-06-06 17:41:58 +0530

    attendance

--- src/main/java/com/example/feesManagementSystem/Repository/FeesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.feesManagementSystem.Repository;
-
-import com.example.feesManagementSystem.Entity.Fees;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeesRepository extends JpaRepository<Fees, Long> {
-}
